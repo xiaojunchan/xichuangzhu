b2fda6b2c06bcf87d71d1f1e94b502889ddc3717
hustlzp <hustlzp@qq.com>
2013-01-30 15:39:40 +0800

    add some pages

--- xichuangzhu.py
+++ xichuangzhu.py
@@ -1,23 +1,41 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import MySQLdb
 
 app = Flask(__name__)
 
-# home page
+conn = MySQLdb.connect(host='localhost', user='root', passwd='xiaowangzi', db='classic', charset='utf8')
+cursor = conn.cursor()
+
+# page home
 @app.route('/')
 def index():
-	#conn = MySQLdb.connect(host='localhost', user='root', passwd='xiaowangzi', db='beforeidie', charset='utf8')
-	#cursor = conn.cursor()
-	#cursor.execute('SELECT * FROM goals')
-	#return cursor.fetchone()[1]
-	return render_template('index.html')
+	cursor.execute('SELECT * FROM work')
+	works = cursor.fetchall()
+	return render_template('index.html', works=works)
 
-# add classic works
-@app.route('/add')
-def fuckass():
-    return '添加作品'
+# page author
+@app.route('/author')
+def author():
+	cursor.execute('SELECT * FROM author')
+	authors = cursor.fetchall()
+	return render_template('author.html', authors=authors)
+
+# page dynasty
+@app.route('/dynasty')
+def dynasty():
+	cursor.execute('SELECT * FROM dynasty')
+	dynasties = cursor.fetchall()
+	return render_template('dynasty.html', dynasties=dynasties)
+
+# page add works
+@app.route('/add_work', methods=['GET', 'POST'])
+def add_work():
+	if request.method == 'GET':
+		return render_template('add_work.html')
+	elif request.method == 'POST':
+		return '添加作品'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)

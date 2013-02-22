8f1a330755dd69814160983eb3e5ecf47ce7debf
hustlzp <hustlzp@qq.com>
2013-02-22 21:12:13 +0800

    update style

--- xichuangzhu/controllers/user.py
+++ xichuangzhu/controllers/user.py
@@ -1,3 +1,5 @@
+#-*- coding: UTF-8 -*-
+
 from flask import render_template, request, redirect, url_for, json
 
 from xichuangzhu import app
@@ -7,6 +9,9 @@ from xichuangzhu.models.user_model import User
 import urllib, urllib2
 
 import smtplib
+from email.mime.text import MIMEText
+
+import hashlib
 
 @app.route('/login/douban')
 def auth():
@@ -29,25 +34,53 @@ def auth():
 	userID = int(info['douban_user_id'])
 	accessToken = info['access_token']
 
-	# get userinfo
-
 	if User.check_user(userID):
 		# if user exist, set session
 		return '1'
 	else:
 		# if not, create a new user and verify email
-		s = smtplib.SMTP('smtp.qq.com', 25)
-		s.login('hustlzp@qq.com', 'xiaowang2013qqzi')
-		error = s.sendmail('hustlzp@qq.com', '724475543@qq.com', "Subject: Hi from Python\n\nHello.")
-		return "s"
-		
+
+		# get user info
 		url = "https://api.douban.com/v2/user/~me"
 		req = urllib2.Request(url)
 		req.add_header('Authorization', 'Bearer ' + accessToken)
 		response = urllib2.urlopen(req)
-		return response.read()
-		return '0'
-		# User.add_user()
+
+		# remove '\' and convert str to dict
+		userInfo = eval(response.read().replace('\\', ''))
+
+		# add user
+		userID = int(userInfo['id'])
+		name = userInfo['name']
+		avatar = userInfo['avatar']
+		signature = userInfo['signature']
+		desc = userInfo['desc']
+		locationID = int(userInfo['loc_id'])
+		location = userInfo['loc_name']
+
+		User.add_user(userID, name, avatar, signature, desc, locationID, location)
+
+		# send email
+		f_addr = "hustlzp@qq.com"
+		t_addr = "724475543@qq.com"
+
+		verify_code = hashlib.sha1(name).hexdigest()
+		verify_url = "http://xichuangzhu.com/user/verify_email/" + userID + "/" + verify_code
+		msgText = '''<html>
+			<h1>点击如下链接激活你在西窗烛的帐号：</h1>
+			<a href='%s'>%s</a>
+			</html>''' % (verify_url, verify_url)
+
+		msg = MIMEText(msgText, 'html', 'utf-8')
+		msg['From'] = f_addr
+		msg['To'] = t_addr
+		msg['Subject'] = "[西窗烛] 邮箱验证"
+
+		s = smtplib.SMTP('smtp.qq.com', 25)
+		s.login('hustlzp@qq.com', 'xiaowang2013qqzi')
+		error = s.sendmail('hustlzp@qq.com', '724475543@qq.com', msg.as_string())
+		return "success"
 		# return redirect(url_for('home'))
 
-	return info['access_token']
+#@app.route('/login/douban/<>')
+#def verify():
--- xichuangzhu/models/user_model.py
+++ xichuangzhu/models/user_model.py
@@ -10,3 +10,14 @@ class User:
 		query = "SELECT * FROM user WHERE UserID = %d" % userID
 		cursor.execute(query)
 		return cursor.rowcount > 0
+
+# NEW
+
+	# add a new user
+	@staticmethod
+	def add_user(userID, name, avatar, signature, desc, locationID, location):
+		query = '''INSERT INTO user (UserID, Name, Avatar, Signature, Description, LocationID, Location)\n
+			VALUES (%d, '%s', '%s', '%s', '%s', %d, '%s')''' % (userID, name, avatar, signature, desc, locationID, location)
+		cursor.execute(query)
+		return conn.commit()
+

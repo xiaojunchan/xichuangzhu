9d484db89cd8c44100a4ef9af26f6eba3d09a578
hustlzp <hustlzp@qq.com>
2013-03-02 15:58:03 +0800

    finish timeline for dynasty

--- xichuangzhu/controllers/dynasty.py
+++ xichuangzhu/controllers/dynasty.py
@@ -1,26 +1,23 @@
-from flask import render_template, request, redirect, url_for, json
+from flask import render_template, request, redirect, url_for, json, render_template_string
 
 from xichuangzhu import app
 
 from xichuangzhu.models.dynasty_model import Dynasty
 from xichuangzhu.models.author_model import Author
 
-# page all dynasties
-#--------------------------------------------------
-
-@app.route('/dynasty')
-def dynasty():
-	dynasties = Dynasty.get_dynasties()
-	return render_template('dynasty.html', dynasties=dynasties)
-
 # page single dynasty
 #--------------------------------------------------
 
-@app.route('/dynasty/<int:dynastyID>')
-def single_dynasty(dynastyID):
-	dynasty = Dynasty.get_dynasty(dynastyID)
-	authors = Author.get_authors_by_dynasty(dynastyID)
-	return render_template('single_dynasty.html', dynasty=dynasty, authors=authors)
+@app.route('/dynasty/<int:dynasty_id>')
+def dynasty(dynasty_id):
+	# gene html code
+	dynasty = Dynasty.get_dynasty(dynasty_id)
+	authors = Author.get_authors_by_dynasty(dynasty_id)
+	dynasty_html = render_template('single_dynasty.widget', dynasty=dynasty, authors=authors)
+
+	# render view
+	dynasties = Dynasty.get_dynasties()
+	return render_template('dynasty.html', dynasty_html=dynasty_html, dynasty_id=dynasty_id, dynasties=dynasties)
 
 # page add dynasty
 #--------------------------------------------------
@@ -50,3 +47,12 @@ def edit_dynasty(dynastyID):
 		endYear      = int(request.form['endYear'])
 		Dynasty.edit_dynasty(dynasty, introduction, startYear, endYear, dynastyID)
 		return redirect(url_for('single_dynasty', dynastyID=dynastyID))
+
+# json - get single dynasty info
+#--------------------------------------------------
+@app.route('/dynasty/json', methods=['POST'])
+def get_dynasty_by_json():
+	dynasty_id = int(request.form['dynasty_id'])
+	dynasty = Dynasty.get_dynasty(dynasty_id)
+	authors = Author.get_authors_by_dynasty(dynasty_id)
+	return render_template('single_dynasty.widget', dynasty=dynasty, authors=authors)
